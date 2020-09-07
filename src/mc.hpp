934ee1d289e84ef3012b9c553f726d38c6a706fa
Yong Hwei Toon <yonghwei11@gmail.com>
2020-09-07 22:32:39 +0000

    added more to MC constructor

--- src/mc.hpp
+++ src/mc.hpp
@@ -11,19 +11,50 @@ class MC : public Question{
     public:
         MC(){
             int intInput = 0;
-            while(intInput < 1 || intInput > 9){
-                std::cout << "Enter number of correct choices (up to 9): ";
+            char charInput = '';
+            std::string strInput = "";
+            while(intInput < 1 || intInput > 9){ //verifies and receives user input for numCorrect
+                std::cout << "Enter number of correct choices (1 to 9):" << std::endl;
+                std::cout << ">> ";
                 std::cin.clear();
                 std::cin.ignore(1000,'\n');
                 std::cin >> intInput;
+                std::endl;
                 if(std::cin.fail()){ //check if input is int
-                    std::cout << "Error: input must be in integer form (1, 2, 3, ...)." << std::endl;
+                    std::cout << "Error: input must be of integer type [1, 2, 3, ...]." << std::endl;
                     std::cout << "Please try again." << std::endl;
                 }
                 else if(intInput < 1 || intInput > 9){
                     std::cout << "Error: integer input must be greater than 0 and less than 10." << std::endl;
                     std::cout << "Please try again." << std::endl;
                 }
+                else
+                    numCorrect = intInput; //numCorrect value set
+            }
+            
+            while(1){
+                std::cout << "Enter question:" << std::endl;
+                std::cout << ">> ";
+                std::cin.clear();
+                std::cin.ignore(1000,'\n');
+                std::getline(std::cin, strInput);
+                std::endl;
+                std::cout << "Enter 'q' to reenter question or 'e' to continue" << std::endl;
+                std::cout << ">> ";
+                std::cin.clear();
+                std::cin.ignore(1000,'\n');
+                std::cin >> charInput;
+                std::endl;
+                if(std::cin.fail()){ //check if input is char
+                    std::cout << "Error: input must be of character type ['a', 'A', 'b', ...]." << std::endl;
+                    std::cout << "Please try again." << std::endl;
+                }
+                else if(charInput != 'q' && charInput != 'e'){
+                    std::cout << "Error: character input must be either 'q' or 'e'." << std::endl;
+                    std::cout << "Please try again." << std::endl;
+                }
+                else
+                    question = strInput;
             }
             //std::cout << "TODO: MC::MC()" << std::endl;
         }
