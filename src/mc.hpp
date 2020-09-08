8b89e3ed279b219fe107b25452fd2cc6727cf7ff
Yong Hwei Toon <yonghwei11@gmail.com>
2020-09-08 01:13:55 +0000

    greatly improved MC class design

--- src/mc.hpp
+++ src/mc.hpp
@@ -8,41 +8,31 @@ class MC : public Question{
         std::string question;
         std::vector<std::string> choices;
         int numCorrect;
+        
     public:
         MC(){
-            int intInput = 0;
+            setNumCorrect();
+            setQuestion();
+            setChoices();
+        }
+
+        std::string getQuestion() const{
+            return question;
+        }
+
+        std::vector<std::string> getChoices() const{
+            return choices;
+        }
+
+        int getNumCorrect() const{
+            return numCorrect;
+        }
+
+        void setQuestion(){
             char charInput = '\0';
             std::string strInput = "";
-            int correctChoiceIndex = 1;
-            int incorrectChoiceIndex = 1;
-            bool choiceTypeFlag = false;
-            while(1){ //receives and verifies user input for int numCorrect
-                //resets temp input variables
-                intInput = 0;
-                charInput = '\0';
-                strInput = "";
-                std::cout << "Enter number of correct choices (1 to 9):" << std::endl;
-                std::cout << ">> ";
-                std::cin.clear();
-                std::cin.ignore(1000,'\n');
-                std::cin >> intInput;
-                std::cout << std::endl;
-                if(std::cin.fail()){ //check if input is int
-                    std::cout << "Error: input must be of integer type [1, 2, 3, ...]." << std::endl;
-                    std::cout << "Please try again." << std::endl;
-                }
-                else if(intInput < 1 || intInput > 9){
-                    std::cout << "Error: integer input must be greater than 0 and less than 10." << std::endl;
-                    std::cout << "Please try again." << std::endl;
-                }
-                else{
-                    numCorrect = intInput; //numCorrect value set
-                    break;
-                }
-            }
             while(1){ //receives and verifies user input for std::string question
                 //resets temp input variables
-                intInput = 0;
                 charInput = '\0';
                 strInput = "";
                 std::cout << "Enter question:" << std::endl;
@@ -74,12 +64,17 @@ class MC : public Question{
                     break;
                 }
             }
+        }
+
+        void setChoices(){
+            char charInput = '\0';
+            std::string strInput = "";
+            bool choiceTypeFlag = false;
             while(1){ //receives and verifies user input for std::vector<std::string> choices
                 //resets temp input variables
-                intInput = 0;
                 charInput = '\0';
                 strInput = "";
-                if(correctChoiceIndex <= numCorrect){ //error here
+                if(correctChoiceIndex <= numCorrect){
                     std::cout << "Enter correct choice (" << correctChoiceIndex << " / " << numCorrect << "):" << std::endl;
                     choiceTypeFlag = true;
                 }
@@ -115,35 +110,36 @@ class MC : public Question{
                         correctChoiceIndex++;
                     else
                         incorrectChoiceIndex++;
-                    choices.pushback(strInput);
+                    choices.push_back(strInput);
                     break;
                 }
             }
-            //std::cout << "TODO: MC::MC()" << std::endl;
-        }
-
-        std::string getQuestion() const{
-            return question;
-        }
-
-        std::vector<std::string> getChoices() const{
-            return choices;
-        }
-
-        int getNumCorrect() const{
-            return numCorrect;
-        }
-
-        void setQuestion(){
-            std::cout << "TODO: void MC::setQuestion()" << std::endl;
-        }
-
-        void setChoices(){
-            std::cout << "TODO: void MC::setChoices()" << std::endl;
         }
 
         void setNumCorrect(){
-            std::cout << "TODO: void MC::setNumCorrect()" << std::endl;
+            int intInput = 0;
+            while(1){ //receives and verifies user input for int numCorrect
+                //resets temp input variables
+                intInput = 0;
+                std::cout << "Enter number of correct choices (1 to 9):" << std::endl;
+                std::cout << ">> ";
+                std::cin.clear();
+                std::cin.ignore(1000,'\n');
+                std::cin >> intInput;
+                std::cout << std::endl;
+                if(std::cin.fail()){ //check if input is int
+                    std::cout << "Error: input must be of integer type [1, 2, 3, ...]." << std::endl;
+                    std::cout << "Please try again." << std::endl;
+                }
+                else if(intInput < 1 || intInput > 9){
+                    std::cout << "Error: integer input must be greater than 0 and less than 10." << std::endl;
+                    std::cout << "Please try again." << std::endl;
+                }
+                else{
+                    numCorrect = intInput; //numCorrect value set
+                    break;
+                }
+            }
         }
 
         void display() const{
